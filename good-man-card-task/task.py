34b107ef452e09ceafad19e2ba389a557f2e78dc
Joy818 <zy-668@qq.com>
2022-03-16 23:41:04 +0800

    finish task framework

---
+++ good-man-card-task/task.py
@@ -0,0 +1,22 @@
+from time import sleep
+
+
+class TaskExecuter:
+  def __init__(self, task) -> None:
+      self.task = task
+      self.success = False
+      self.cuase = None
+
+  def execute(self):
+    params = self.task.get_dict_params()
+    print(f'开始执行任务 {params}')
+    sleep(10)
+    print(f'任务执行结束 {params}')
+    self.success = True
+
+  def is_success(self):
+    return self.success
+
+
+  def get_cause(self):
+    return self.cause
